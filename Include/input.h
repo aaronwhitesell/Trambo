34243fdca1b8b3aca1e504613618f02540c39598
Aaron Whitesell <axwhitesell@gmail.com>
2014-02-25 19:14:56 +0300

    Initial commit.

---
+++ Include/input.h
@@ -0,0 +1,13 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+namespace trmb
+{
+	class Input
+	{
+	public:
+		int static add(int x, int y);
+	};
+}
+
+#endif
---
+++ Source/Trambo/Trambo/Source/input.cpp
@@ -0,0 +1,7 @@
+#include "../../../../Include/input.h"
+
+
+int trmb::Input::add(int x, int y)
+{
+	return x + y;
+}
